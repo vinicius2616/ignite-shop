02b8b38421513a2ddc6196605978bd82592793e3
Vinicius Faleiros <vinicius.faleiros@hotmail.com>
2025-01-23 19:02:24 -0300

    implement SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { HomeContainer, Product } from '@/styles/pages/home'
 
 import { stripe } from '@/lib/stripe'
 import 'keen-slider/keen-slider.min.css'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Stripe from 'stripe'
 
 interface HomeProps {
@@ -42,7 +42,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price'],
   })
@@ -61,5 +61,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 2,
   }
 }
